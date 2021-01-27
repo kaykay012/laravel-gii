634d45afdad30c1d1ded99baa8eaa944af784b3b
kai-company <Administrator@Kai-Company-PC>
2021-01-27 10:00:00 +0800

    view blade 优化

--- src/view-blade/form.blade.php
+++ src/view-blade/form.blade.php
@@ -10,7 +10,7 @@
 
 @section('content')
 <div class="st-h15"></div>
-<form class="layui-form" action="" lay-filter="ST-FORM" id="ST-FORM">
+<form class="layui-form" action="">
     <input name="id" type="hidden" value="{{$model->id}}" />
     <div class="layui-row">
         <div class="layui-col-xs11">DummyInputParams
@@ -28,11 +28,7 @@
 @push('scripts_bottom')
 <script>
     !function () {
-        //监听提交
-        layui.form.on('submit(ST-SUBMIT)', function (data) {
-            Util.postForm('ST-FORM', data.field, true);
-            return false;
-        });
+        //...
     }();
 </script>
 @endpush
